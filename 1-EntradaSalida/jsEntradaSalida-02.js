ba3eb037e85510fdf3bb1922058514b58c902324
ElianTemes <eliantemes1303@gmail.com>
2021-02-01 15:16:24 -0300

    Ejercicio Resuelto (aclaraciones)

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,10 +1,10 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-function mostrar()
+function mostrar() // Funcion mostrar, creada por el profesor, el codigo va dentro del bloque cerrado por llaves
 {
-	let userInput =
-	prompt ("Ingrese su nombre:"); 
+	let userInput; //Const es una variable de solo lectura, solo se puede guardar algo al declarar y despues no se puede modificar
+	userInput = prompt ("Ingrese su nombre:"); // Declaro la variable userInput con lo que ingrese el usuario por prompt, la funcion prompt al cerrar funciona de manera equivalente a poner userInput = "nombre"
 	alert(userInput);
 }
 
