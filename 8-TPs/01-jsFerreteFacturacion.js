055b525f70af349b2e9067f4f406d97490f4dcf9
ElianTemes <eliantemes1303@gmail.com>
2021-02-04 01:34:13 -0300

    Resuelto

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -4,14 +4,75 @@ B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
-{
-	
+{   
+    let prod1; 
+    let prod2; 
+    let prod3;
+    let resultadoSum;
+
+    prod1 = document.getElementById('txtIdPrecioUno').value;
+    prod2 = document.getElementById('txtIdPrecioDos').value;
+    prod3 = document.getElementById('txtIdPrecioTres').value;
+    prod1 = parseInt(prod1, 10);
+    prod2 = parseInt(prod2, 10);
+    prod3 = parseInt(prod3, 10);
+
+    resultadoSum = prod1 + prod2 + prod3; 
+
+    alert(`El resultado de la suma es: ${resultadoSum}`)
+
+    document.getElementById('txtIdPrecioUno').value = ''
+    document.getElementById('txtIdPrecioDos').value = ''
+    document.getElementById('txtIdPrecioTres').value = ''
+
 }
 function Promedio () 
-{
+{   
+    let prod1; 
+    let prod2; 
+    let prod3; 
+    let resultadoProm;
+
+    prod1 = document.getElementById('txtIdPrecioUno').value;
+    prod2 = document.getElementById('txtIdPrecioDos').value;
+    prod3 = document.getElementById('txtIdPrecioTres').value;
+    prod1 = parseInt(prod1, 10);
+    prod2 = parseInt(prod2, 10);
+    prod3 = parseInt(prod3, 10);
+
+    resultadoProm = (prod1 + prod2 + prod3) / 3; 
+
+    alert(`El promedio es: ${resultadoProm}`)
+
+    document.getElementById('txtIdPrecioUno').value = ''
+    document.getElementById('txtIdPrecioDos').value = ''
+    document.getElementById('txtIdPrecioTres').value = ''
 	
 }
 function PrecioFinal () 
-{
+{   
+    let prod1; 
+    let prod2; 
+    let prod3;
+    let prodAll; 
+    let iva;
+    let resultadoFin;
+
+    prod1 = document.getElementById('txtIdPrecioUno').value;
+    prod2 = document.getElementById('txtIdPrecioDos').value;
+    prod3 = document.getElementById('txtIdPrecioTres').value;
+    prod1 = parseInt(prod1, 10);
+    prod2 = parseInt(prod2, 10);
+    prod3 = parseInt(prod3, 10);
+
+    prodAll = prod1 + prod2 + prod3;
+    iva = prodAll / 100 * 21; 
+    resultadoFin = iva + prodAll;
+
+    alert(`El precio final con IVA es: ${resultadoFin}`)
+
+    document.getElementById('txtIdPrecioUno').value = ''
+    document.getElementById('txtIdPrecioDos').value = ''
+    document.getElementById('txtIdPrecioTres').value = ''
 	
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,46 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    let largo;
+    let ancho;
+    let perimetro;
+    let alambre;
 
+    largo = document.getElementById('txtIdLargo').value;
+    ancho = document.getElementById('txtIdAncho').value;
+    perimetro = ( largo * 2 ) + ( ancho * 2 );
+    alambre = perimetro * 3;
+
+    alert(`Se necesitan ${alambre}mt de alambre`);
+
+    document.getElementById('txtIdLargo').value = '';
+    document.getElementById('txtIdAncho').value = '';
 }
 function Circulo () 
 {
-	
+    let radio;
+    let circunferencia;
+    let alambre;
+
+    radio = document.getElementById('txtIdRadio').value;
+    circunferencia = ( radio * 2 ) * 3.14;
+    alambre = circunferencia * 3;
+    
+    alert(`Se necesitan ${alambre}mt de alambre`);
+
+    document.getElementById('txtIdRadio').value = '';
+
 }
 function Materiales () 
 {
-	
+    let largo = parseInt(document.getElementById('txtIdLargo').value);
+    let ancho = parseInt(document.getElementById('txtIdAncho').value);
+    let cal = 3 * (largo * ancho);
+    let cemento = 2 * (largo * ancho);
+
+    alert(`Se necesitan ${cemento} bolsas de cemento y ${cal} bolsas de cal para todo el contrapiso`);
+
+    document.getElementById('txtIdLargo').value = '';
+    document.getElementById('txtIdAncho').value = '';
+    
 }
